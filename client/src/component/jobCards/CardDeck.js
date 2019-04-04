f4599ba1898e27374d98145487ff030143528dab
Vinit Vachhani <vinitvachhani@gmail.com>
2019-04-04 18:57:30 -0400

    changes

--- client/src/component/jobCards/CardDeck.js
+++ client/src/component/jobCards/CardDeck.js
@@ -4,12 +4,13 @@ import Card from './Card'
 
 export default class CardDeck extends Component {
   render() {
+    console.log(this.props);
     let {cards} = this.props
     return <div className="CardDeck">
       {cards && cards.slice(0).reverse().map((card, idx)=> {
         // return <Card imgUrl={card} idx={card.substring(1, card.length - 4)} shiftCard={this.props.shiftCard} key={idx} />
         // return <Card  name = {card.name} title ={card.title} tech = {card.tech} company = {card.company} ></Card>
-        return <Card  name = {card.company} title ={card.description} tech = {card.requirements} ></Card>
+        return <Card  name = {card.company} title ={card.industry} tech = {card.requirements} ></Card>
       })}
     </div>
   }
